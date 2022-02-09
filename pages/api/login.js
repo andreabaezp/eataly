8a29d31a5b2c44100e02b90c2fc5ca7a6ba416e2
andreabaezp <abaezplazas@gmail.com>
2022-02-08 22:53:54 -0500

    debug logs

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,6 +3,11 @@ import cookie from "cookie";
 const handler = (req, res) => {
   if (req.method === "POST") {
     const { username, password } = req.body;
+    console.log(username);
+    console.log(password);
+    console.log(process.env.ADMIN_USERNAME)
+    console.log(process.env.ADMIN_PASSWORD)
+    
     if (
       username === process.env.ADMIN_USERNAME &&
       password === process.env.ADMIN_PASSWORD
